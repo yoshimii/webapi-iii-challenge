222cb8e90171a44981efdb9be42cada8a06cead8
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-02 17:58:05 -0500

    user router

--- server.js
+++ server.js
@@ -1,21 +1,20 @@
 const express = require('express');
 const server = express();
 const helmet = require('helmet');
+const userRouter = require('./users/userRouter');
 
+// import middleware
 const logger = require('./middleware/logger-middleware');
 
 //set up global middleware
-
 server.use(logger);
 server.use(helmet());
-server.use(express.json())
-
-
-
-
+server.use(express.json());
+server.use('/', userRouter);
 
 server.get('/', (req, res) => {
   res.send(`<h2>Let's write some middleware!</h2>`)
 });
+
 
 module.exports = server;
