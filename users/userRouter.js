4bb1ac1d2d3994a0b65750fd7e444d47fa4aa7ff
Emma Zarate <2327788+yoshimii@users.noreply.github.com>
2019-10-02 17:58:36 -0500

    get request okay

--- users/userRouter.js
+++ users/userRouter.js
@@ -1,47 +1,43 @@
-const express = 'express';
-
+const express = require('express');
+// import middleware
+const validateUserId = require('../middleware/validate-middleware');
+//import userDb module to use user methods
+const User = require('./userDb');
 const router = express.Router();
 
 router.post('/', (req, res) => {
 
 });
 
-router.post('/:id/posts', (req, res) => {
-
+router.post('/:id/posts', validateUserId, (req, res) => {
+    const id = req.params.id;
+    User.getById(id).then(res => {
+      res.status(201).json(res);
+    })
 });
 
 router.get('/', (req, res) => {
 
 });
 
-router.get('/:id', (req, res) => {
-
+router.get('/:id', validateUserId, (req, res) => {
+    const id = req.params.id;
+    User.getById(id).then(user => {
+        res.status(200).json(user);
+    })
 });
 
-router.get('/:id/posts', (req, res) => {
+router.get('/:id/posts', validateUserId, (req, res) => {
 
 });
-
-router.delete('/:id', (req, res) => {
-
+ 
+router.delete('/:id', validateUserId, (req, res) => {
+// 
 });
 
 router.put('/:id', (req, res) => {
 
 });
 
-//custom middleware
-
-function validateUserId(req, res, next) {
-
-};
-
-function validateUser(req, res, next) {
-
-};
-
-function validatePost(req, res, next) {
-
-};
 
 module.exports = router;
